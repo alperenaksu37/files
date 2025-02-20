4603c6f5548fafcb712825b97a9ca087d6fb43cf
alperenaksu37 <alperenaksu372010@gmail.com>
2025-02-20 21:16:38 +0300

    Converted to a plain text

--- captcha/verify.ps1
+++
@@ -1,5 +0,0 @@
-Write-Output "⚠️  DİKKAT: SAHTE reCAPTCHA TUZAĞINA DÜŞTÜNÜZ! ⚠️"
-Write-Output "🚨 Gerçek Google reCAPTCHA sizden Çalıştır'a bir şey yapıştırmanızı asla istemez!"
-Write-Output "📌 Bir daha lütfen robot doğrulaması için Windows + R tuşlarının kullanılmadığını anlayın."
-Write-Output "🔒 Bilgisayarınızı koruyun ve şüpheli komutları çalıştırmadan önce kontrol edin!"
-pause
