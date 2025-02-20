dde2910192fee27204defd4c2ec6041b3dcf486e
alperenaksu37 <alperenaksu372010@gmail.com>
2025-02-20 20:53:28 +0300

    Update and rename verify.cmd to verify.ps1

---
+++ captcha/verify.ps1
@@ -0,0 +1,5 @@
+echo "⚠️  DİKKAT: SAHTE reCAPTCHA TUZAĞINA DÜŞTÜNÜZ! ⚠️"
+echo "🚨 Gerçek Google reCAPTCHA sizden Çalıştır'a bir şey yapıştırmanızı asla istemez!"
+echo "📌 Bir daha lütfen robot doğrulaması için Windows + R tuşlarının kullanılmadığını anlayın."
+echo "🔒 Bilgisayarınızı koruyun ve şüpheli komutları çalıştırmadan önce kontrol edin!"
+pause
